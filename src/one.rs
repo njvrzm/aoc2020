e9f2cd537ed5e344914ec5c1473b8103f40478b2
Nathan Verzemnieks <njvrzm@gmail.com>
2020-12-02 11:31:43 -0800

    Refactoring one

--- src/one.rs
+++ src/one.rs
@@ -1,9 +1,37 @@
 pub mod day_one {
-    pub fn run(input: String) {
+    fn parse_lines(input: String) -> Vec<usize> {
         let mut nums = Vec::new();
 
         for line in input.lines() {
-            nums.push(line.parse::<i32>().unwrap());
+            nums.push(line.parse::<usize>().unwrap());
+        }
+        nums.sort();
+        nums
+    }
+    pub fn run_one(input: String) {
+        let mut calc = 0;
+        let nums = parse_lines(input);
+        for one in nums.iter() {
+            for two in nums.iter() {
+                // These shortcuts take the total number of inner loops from 40622 to 776
+                calc += 1;
+                if one + two > 2020 {
+                    break
+                }
+                if one + two == 2020 {
+                    println!("{} + {} == 2020; {} * {} == {}", one, two, one, two, one * two);
+                    println!("Total calculations: {}", calc);
+                    return
+                }
+            }
+        }
+    }
+
+    pub fn run_two(input: String) {
+        let mut nums = Vec::new();
+
+        for line in input.lines() {
+            nums.push(line.parse::<usize>().unwrap());
         }
         nums.sort();
         let mut calc = 0;
@@ -11,11 +39,11 @@ pub mod day_one {
             for two in nums.iter() {
                 // These shortcuts take the total number of inner loops from 40622 to 776
                 if one + two > 2020 {
-                    continue
+                    break
                 }
                 for three in nums.iter() {
                     if one + two + three > 2020 {
-                        continue
+                        break
                     }
                     calc += 1;
                     if one + two + three == 2020 {
